faff800cb470b4a0e9480ee175da3c1e891c9072
TyuringPC <tehb23@gmail.com>
2021-04-12 21:54:01 +0300

    Button added
    Add button, after end of select. On button clik, text speak

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 'use strict';
 let synth = window.speechSynthesis;
 
-const text = document.querySelector('body');
+const text = document.querySelector('html');
+text.classList.add('main');
 
-text.addEventListener("mouseup", textShow);
+text.addEventListener("mouseup", createButton);
 
-
-//Передаем в переменную speech текст и после его проговариваем
-function textShow() {
-    let speech = new SpeechSynthesisUtterance(document.getSelection().toString());
+function createButton() {
+    text.removeEventListener("mouseup", createButton);
+    const $btn = document.createElement('BUTTON');
+    $btn.textContent = "Произнести";
+    text.appendChild($btn);
+    $btn.onclick = function () {
+        let speech = new SpeechSynthesisUtterance(document.getSelection().toString());
      
     synth.speak(speech);
+    };
 };
