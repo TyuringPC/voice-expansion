499053214dde353b497565523a91c518420092f9
TyuringPC <tehb23@gmail.com>
2021-04-20 18:27:51 +0300

    Popup button added

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
 'use strict';
-let synth = window.speechSynthesis;
+const synth = window.speechSynthesis;
+const $doc = document.querySelector('html');
+const $body = document.querySelector('body');
+const $ul = document.createElement('ul');
 
-const text = document.querySelector('html');
-text.classList.add('main');
+$ul.classList.add('context-menu');
+$ul.style.cssText = `
+    margin: 0;
+    padding: 0;
+    position: fixed;
+    list-style: none;
+    background: #fff;
+    border: 1px solid #000;
+    border-radius: 1px;
+    display: none;
+`
+const $li = document.createElement('li');
+$li.style.cssText = `
+    width: 20%;
+    padding: 0px;
+    box-sizing: border-box;
+    cursor: pointer;
+    font-size: 16px;
+`
+$li.innerText = 'Озвучить';
+ 
+$doc.addEventListener("mouseup", createLi);
 
-text.addEventListener("mouseup", createButton);
+    
+function createLi(){
+    $ul.addEventListener("mouseup", event =>{
+        event.stopPropagation();
+    },false);
 
-function createButton() {
-    text.removeEventListener("mouseup", createButton);
-    const $btn = document.createElement('BUTTON');
-    $btn.textContent = "Произнести";
-    text.appendChild($btn);
-    $btn.onclick = function () {
-        let speech = new SpeechSynthesisUtterance(document.getSelection().toString());
-     
-    synth.speak(speech);
+    const text = document.getSelection().toString();
+    const speech = new SpeechSynthesisUtterance(text);
+
+    $doc.removeEventListener("mouseup", createLi);
+    $body.appendChild($ul);
+    $ul.appendChild($li);
+    
+    $ul.style.display = "block";
+    $ul.style.top = event.clientY - 30 + "px";
+    $ul.style.left = event.clientX - 15 + "px";
+    $li.onclick = function() {
+        
+        synth.speak(speech); 
+        $ul.style.display = "none";
+        $ul.remove();
     };
+    $doc.addEventListener("mouseup", createLi);
 };
+
